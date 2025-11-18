e13df9850af1afa70bf2bce0f89bb6a9295fa434
tobitube <mdparvezh217@gmail.com>
2025-11-18 13:46:23 +0600

    Update main.py

--- main.py
+++ main.py
@@ -31,4 +31,4 @@ if __name__ == "__main__":
     bot_thread.start()
     
     # Start web server
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=8080, debug=False)
